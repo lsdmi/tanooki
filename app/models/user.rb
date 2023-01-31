3e1c5f784bbb5b7d08c2509eaac3a3b39da20d39
Dmytro Liaska <dmytro.liaska@homestars.com>
2023-01-31 13:55:10 -0500

    registration validation, notice styles, containers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, length: { minimum: 3, maximum: 10 }, format: { with:  /\A[a-zA-Z0-9 ]+\z/ }
 end
