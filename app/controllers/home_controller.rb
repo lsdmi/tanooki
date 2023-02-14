a120e5d00c459f6ff88fb8f0409f48b34b55bec4
Dmytro Liaska <dmytro.liaska@homestars.com>
2023-02-14 11:39:20 -0500

    revert some changes in bin folder, homepage redesign

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@
 class HomeController < ApplicationController
   def index
     @highlight = Tale.highlight || Tale.last
-    @pagy, @tales = pagy_countless(Tale.all.order(created_at: :desc).excluding(@highlight), items: 9)
+    @top4 = Tale.all.order(created_at: :desc).excluding(@highlight).first(4)
+    @pagy, @tales = pagy_countless(Tale.all.order(created_at: :desc).excluding(@highlight, @top4), items: 9)
 
     render 'scrollable_list' if params[:page]
   end
