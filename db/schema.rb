4cae4ef91b41fa904a175ab18d4621253f9ddb15
Dmytro Liaska <dmytro.liaska@homestars.com>
2023-03-05 18:00:16 -0500

    trying to resolve various ci config issues
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues
    
    trying to resolve various ci config issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_02_27_205238) do
-  create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "action_text_rich_texts", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "body", size: :long
     t.string "record_type", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_205238) do
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
   end
 
-  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -33,7 +33,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_205238) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -46,13 +46,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_205238) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "advertisements", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "advertisements", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "slug", null: false
     t.string "caption", null: false
     t.string "resource", null: false
@@ -63,7 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_205238) do
     t.index ["slug"], name: "index_advertisements_on_slug", unique: true
   end
 
-  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "parent_id"
     t.bigint "publication_id", null: false
@@ -75,7 +75,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_205238) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "publications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "publications", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "slug", null: false
     t.string "type", null: false
     t.string "title", null: false
@@ -91,7 +91,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_205238) do
     t.index ["user_id"], name: "index_publications_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "name", null: false
