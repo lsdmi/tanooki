eb508963aa07711850c0240aff23a89e05243164
Dmytro Liaska <dmytro.liaska@homestars.com>
2023-05-01 09:18:10 -0400

    small system update
    small system update

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,5 +8,4 @@ pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin 'flowbite', to: 'https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.turbo.min.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
-pin 'trix'
 pin '@rails/actiontext', to: 'actiontext.js'
