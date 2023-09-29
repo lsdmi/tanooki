b47047a7afcf38f9acaf287765cec8fc5e6fa578
Dmytro Liaska <dmytro.liaska@homestars.com>
2023-09-28 20:43:20 -0400

    rails 7.0.8
    more bundle updates
    
    more bundle updates

--- test/jobs/telegram_job_test.rb
+++ test/jobs/telegram_job_test.rb
@@ -4,15 +4,15 @@ require 'test_helper'
 
 class TelegramJobTest < ActiveSupport::TestCase
   test 'perform sends message in production' do
-    rails_env_mock = MiniTest::Mock.new
+    rails_env_mock = Minitest::Mock.new
     rails_env_mock.expect(:production?, true)
 
     Rails.stub(:env, rails_env_mock) do
       object = mock_telegram_object
 
       TelegramBot.stub(:init, nil) do
-        bot_mock = MiniTest::Mock.new
-        api_mock = MiniTest::Mock.new
+        bot_mock = Minitest::Mock.new
+        api_mock = Minitest::Mock.new
 
         api_mock.expect(:send_message, nil) do |params|
           assert_equal '@bakaInUa', params[:chat_id]
@@ -33,7 +33,7 @@ class TelegramJobTest < ActiveSupport::TestCase
   end
 
   def mock_telegram_object
-    mock_object = MiniTest::Mock.new
+    mock_object = Minitest::Mock.new
     mock_object.expect(:telegram_message, 'Test message')
     mock_object.expect(:telegram_message, 'Test message')
     mock_object
