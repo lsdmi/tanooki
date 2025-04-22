d5c89e44c9bb0ac1e311cb0bdb7f9914866ca3c8
Dmytro Liaska <admin@ip-10-0-0-150.eu-west-1.compute.internal>
2025-04-22 19:17:43 -0400

    font size change

--- app/javascript/font_toggler.js
+++ app/javascript/font_toggler.js
@@ -1,32 +1,50 @@
 const FONT_CLASSES = ['', 'font-oswald', 'font-georgia', 'font-helvetica', 'font-times'];
+const FONT_SIZE_CLASSES = ['', 'text-sm', 'text-base', 'text-lg', 'text-xl'];
 
 const initializeFontToggler = () => {
   const content = document.getElementById('user-content');
 
   if (!content) return;
 
-  let currentIndex = 0;
+  let currentFontIndex = FONT_CLASSES.indexOf(localStorage.getItem('selected-font'));
+  if (currentFontIndex === -1) currentFontIndex = 0;
+
+  let currentSizeIndex = FONT_SIZE_CLASSES.indexOf(localStorage.getItem('selected-font-size'));
+  if (currentSizeIndex === -1) currentSizeIndex = 0;
 
   const toggleFont = () => {
-    currentIndex = (currentIndex + 1) % FONT_CLASSES.length;
-    localStorage.setItem('selected-font', FONT_CLASSES[currentIndex]);
+    currentFontIndex = (currentFontIndex + 1) % FONT_CLASSES.length;
+    localStorage.setItem('selected-font', FONT_CLASSES[currentFontIndex]);
+    applyFontStyles(content);
+  };
 
+  const toggleFontSize = () => {
+    currentSizeIndex = (currentSizeIndex + 1) % FONT_SIZE_CLASSES.length;
+    localStorage.setItem('selected-font-size', FONT_SIZE_CLASSES[currentSizeIndex]);
     applyFontStyles(content);
   };
 
   const fontToggleButton = document.getElementById('font-toggle');
-  fontToggleButton.addEventListener('click', toggleFont);
+  if (fontToggleButton) fontToggleButton.addEventListener('click', toggleFont);
+
+  const fontSizeToggleButton = document.getElementById('font-size-toggle');
+  if (fontSizeToggleButton) fontSizeToggleButton.addEventListener('click', toggleFontSize);
 
   applyFontStyles(content);
 };
 
 const applyFontStyles = (element) => {
   const savedFont = localStorage.getItem('selected-font');
+  const savedFontSize = localStorage.getItem('selected-font-size');
 
-  FONT_CLASSES.filter((cls) => cls).forEach((cls) => element.classList.remove(cls));
+  FONT_CLASSES.filter(Boolean).forEach((cls) => element.classList.remove(cls));
+  FONT_SIZE_CLASSES.filter(Boolean).forEach((cls) => element.classList.remove(cls));
 
   if (savedFont && savedFont !== '') {
     element.classList.add(savedFont);
+  }
+  if (savedFontSize && savedFontSize !== '') {
+    element.classList.add(savedFontSize);
   }
 
   Array.from(element.children).forEach((child) => applyFontStyles(child));
