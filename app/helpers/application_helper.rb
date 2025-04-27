7f2fe27c8d0ffb54abbd6670e93ce14948de32d0
Dmytro Liaska <dmytro.liaska@instapro.com>
2025-04-27 14:15:51 -0400

    dark mode fictions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,6 +43,7 @@ module ApplicationHelper
     (controller_name == 'tales' && action_name == 'show') ||
       (controller_name == 'chapters' && action_name == 'show') ||
       (controller_name == 'scanlators' && action_name == 'show') ||
-      (controller_name == 'chapters_calendar') || (controller_name == 'home')
+      (controller_name == 'chapters_calendar') || (controller_name == 'home') ||
+      (controller_name == 'fictions')
   end
 end
--- app/javascript/controllers/fiction_picker_controller.js
+++ app/javascript/controllers/fiction_picker_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
 
     // Remove highlight from all images
     this.imageTargets.forEach(img => {
-      img.classList.remove('ring-2', 'ring-stone-500')
+      img.classList.remove('ring-2', 'ring-stone-500', 'dark:ring-gray-300')
     })
 
     // Add highlight to the clicked image
@@ -53,6 +53,6 @@ export default class extends Controller {
   }
 
   highlightImage(image) {
-    image.classList.add('ring-2', 'ring-stone-500')
+    image.classList.add('ring-2', 'ring-stone-500', 'dark:ring-gray-300')
   }
 }
