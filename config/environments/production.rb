1f61a875c1da94820aa06dc0a8c760b05fe39e35
Dmytro Liaska <dmytro.liaska@instapro.com>
2025-07-20 16:35:00 -0400

    Fix production issues: reduce cache time from 1 year to 1 hour, fix session expiration from 10 years to 30 days

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
 
-  config.public_file_server.headers = { 'Cache-Control': 'public, max-age=31536000' }
+  config.public_file_server.headers = { 'Cache-Control': 'public, max-age=3600' }
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-Rails.application.config.session_store :cookie_store, key: '_tanooki_session', expire_after: 10.years
+Rails.application.config.session_store :cookie_store, key: '_tanooki_session', expire_after: 30.days
