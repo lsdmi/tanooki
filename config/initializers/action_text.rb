7a74456eec32cfc432c972a1c78874f039d7d5cc
Dmytro Liaska <dmytro.liaska@instapro.com>
2025-08-12 08:00:56 -0400

    Add img tag and image-related attributes to ActionText configuration

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -3,7 +3,7 @@
 Rails.application.config.after_initialize do
   # Set allowed tags for ActionText (Rails 8 approach)
   ActionText::ContentHelper.allowed_tags = %w[
-    p iframe div span br hr blockquote pre code
+    p iframe div span br hr blockquote pre code img
     sub sup mark del ins h1 h2 h3 h4 h5 h6 ul ol li
     b strong i em u small big strike s
   ]
@@ -11,6 +11,7 @@ Rails.application.config.after_initialize do
   # Set allowed attributes for ActionText (Rails 8 approach)
   ActionText::ContentHelper.allowed_attributes = [
     'abbr', 'alt', 'cite', 'class', 'datetime', 'height', 'href', 'lang',
-    'name', 'src', 'style', 'title', 'width', 'xml:lang', 'frameborder', 'allowfullscreen'
+    'name', 'src', 'style', 'title', 'width', 'xml:lang', 'frameborder', 'allowfullscreen',
+    'loading', 'decoding', 'sizes', 'srcset'
   ]
 end
