39e7b3027b4230cbb0be396730e096b706607029
Dmytro Liaska <dmytro.liaska@angi.com>
2025-08-16 20:16:45 -0400

    Redesign fiction details layout with creative badges, responsive centering, and enhanced ActionText support

--- config/initializers/action_text.rb
+++ config/initializers/action_text.rb
@@ -5,13 +5,18 @@ Rails.application.config.after_initialize do
   ActionText::ContentHelper.allowed_tags = %w[
     p iframe div span br hr blockquote pre code img
     sub sup mark del ins h1 h2 h3 h4 h5 h6 ul ol li
-    b strong i em u small big strike s
+    b strong i em u small big strike s a table tr td th
+    thead tbody caption colgroup col details summary
+    figure figcaption time address article aside footer
+    header main nav section
   ]
 
   # Set allowed attributes for ActionText (Rails 8 approach)
   ActionText::ContentHelper.allowed_attributes = [
     'abbr', 'alt', 'cite', 'class', 'datetime', 'height', 'href', 'lang',
     'name', 'src', 'style', 'title', 'width', 'xml:lang', 'frameborder', 'allowfullscreen',
-    'loading', 'decoding', 'sizes', 'srcset'
+    'loading', 'decoding', 'sizes', 'srcset', 'target', 'rel', 'download', 'hreflang',
+    'type', 'media', 'data-*', 'aria-*', 'role', 'tabindex', 'accesskey', 'contenteditable',
+    'dir', 'hidden', 'id', 'spellcheck', 'translate', 'itemscope', 'itemtype', 'itemprop'
   ]
 end
