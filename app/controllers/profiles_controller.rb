0d9ba16c3f29cfb4a7175d49dbba6ea2beb0ccfa
Dmytro Liaska <dmytro.liaska@angi.com>
2025-08-22 14:21:50 -0400

    Refactor profiles controller: move logic from view to controller and reduce ABC complexity

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,8 +5,8 @@ class ProfilesController < ApplicationController
   before_action :pokemon_appearance, only: [:show]
 
   def show
-    # The user is already set by the before_action
-    # Additional logic for profile display can be added here
+    load_user_data
+    load_pokemon_stats
   end
 
   private
@@ -17,5 +17,30 @@ class ProfilesController < ApplicationController
     return if @user
 
     redirect_to root_path
+  end
+
+  def load_user_data
+    @recent_publications = @user.publications.includes(:cover_attachment, :rich_text_description).recent.limit(3)
+    @user_scanlators = @user.scanlators
+    @recent_comments = @user.comments.order(created_at: :desc).includes(:commentable).limit(3)
+    @recent_readings = @user.readings.includes(fiction: :cover_attachment,
+                                               chapter: {}).order(updated_at: :desc).limit(4)
+  end
+
+  def load_pokemon_stats
+    @pokemon_count = @user.pokemons.count
+    @total_pokemon = Pokemon.where(descendant_level: 0).count
+    @victories = calculate_victories
+    @total_battles = calculate_total_battles
+    @user_rating = User.dex_leaders.index(@user) + 1
+  end
+
+  def calculate_victories
+    @user.attacker_battle_logs.where(winner: @user).count +
+      @user.defender_battle_logs.where(winner: @user).count
+  end
+
+  def calculate_total_battles
+    @user.attacker_battle_logs.count + @user.defender_battle_logs.count
   end
 end
