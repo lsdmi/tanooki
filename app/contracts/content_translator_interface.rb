d825fc84d63e3b9e3417345408129ed7958981c5
Dmytro Liaska <dmytro.liaska@angi.com>
2025-10-14 12:51:05 -0400

    Remove blog scraper functionality and related services

--- app/contracts/content_translator_interface.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# Interface for content translation services
-# This follows the Dependency Inversion Principle
-module ContentTranslatorInterface
-  def translate(article_text, title, tags)
-    raise NotImplementedError, "#{self.class} must implement #translate"
-  end
-end
