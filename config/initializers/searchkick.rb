08138228dac594946a2fd5d464c638e40c1d13bc
Dmytro Liaska <dmytro.liaska@angi.com>
2026-05-22 18:32:31 -0400

    Read Elasticsearch URL from ELASTICSEARCH_URL in production.

--- config/initializers/searchkick.rb
+++ config/initializers/searchkick.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 if Rails.env.production?
+  credentials = [
+    ENV.fetch('ELASTICSEARCH_USER', nil),
+    ENV.fetch('ELASTICSEARCH_PASSWORD', nil)
+  ].join(':')
+
   Searchkick.client_options = {
-    url: 'http://167.71.63.106:9200',
+    url: ENV.fetch('ELASTICSEARCH_URL'),
     transport_options: {
-      headers: { 'Authorization' => "Basic #{Base64.strict_encode64("#{ENV.fetch('ELASTICSEARCH_USER',
-                                                                                 nil)}:#{ENV.fetch(
-                                                                                   'ELASTICSEARCH_PASSWORD', nil
-                                                                                 )}")}" }
+      headers: { 'Authorization' => "Basic #{Base64.strict_encode64(credentials)}" }
     }
   }
 end
